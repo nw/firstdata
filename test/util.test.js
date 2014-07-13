cc214260c84c7bd375122e0c67a0b77d2e8ed4ba
Nathan White <nw@nwhite.net>
2014-07-13 03:20:24 -0600

    tested all utils

--- test/util.test.js
+++ test/util.test.js
@@ -1,55 +1,61 @@
-var utils = require('../lib/utils');
+var utils = require('../').utils;
 
-describe('Utilities', function(){
-  
+
+describe('utils.merge', function(){
+
   it('merge should exist', function(){
     utils.merge.should.be.ok;
   });
-  
+
   it('should merge', function(){
     var obj1 = {test: 'me'}
       , obj2 = {test: 'this'}
       , output = utils.merge(obj1, obj2);
-    
+
     output.test.should.eql('this');
   });
-  
+
   it('should handle non objects properly', function(){
     var obj1 = {test: 'me'}
       , output = utils.merge(obj1, null);
-    
+
     output.test.should.eql('me');
   });
-  
+
   it('should merge into nothing', function(){
     var output = utils.merge(null, {test: 'this'});
-    
+
     output.test.should.eql('this');
   });
-  
+
   it('should merge nested objects properly', function(){
     var obj1 = { test: 'me', nest: { prop: 1}}
       , obj2 = { test: 'this', nest: { prop: 2}}
       , output = utils.merge(obj1, obj2);
-    
+
     output.should.eql(obj1);  
     output.test.should.eql('this');
     output.nest.prop.should.eql(2);
   });
-  
+
   it('should merge all arguments together', function(){
     var output = utils.merge({test: 'me'}, null, {test: 'this'}, {passed: true});
-    
+
     output.test.should.eql('this');
     output.passed.should.eql(true);
-    
+
   });
+
+});
+
+
+describe('utils.isObj', function(){
   
-  it('isObj should return true for object', function(){
+  it('should return true for object', function(){
     utils.isObj({}).should.be.ok;
   });
-  
-  it('isObj should return false for everything else', function(){
+
+  it('should return false for everything else', function(){
     utils.isObj(true).should.be.false;
     utils.isObj(3).should.be.false;
     utils.isObj("data").should.be.false;
@@ -57,5 +63,4 @@ describe('Utilities', function(){
     utils.isObj([1,2,3]).should.be.false;
   })
   
-  
-})
+});
